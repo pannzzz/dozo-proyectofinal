e6dbe8ed3eff4912101675ce9f569dc5c255f737
pannzzz <maria.jimenez544@soy.sena.edu.co>
2024-11-09 23:24:21 -0500

    se añaden detalles al menu

--- dozo/src/components/SideMenu.js
+++ dozo/src/components/SideMenu.js
@@ -1,37 +1,61 @@
-    // src/components/SideMenu.js
-    import React from 'react';
-    import '../styles/SideMenu.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css'; // Importar Bootstrap Icons
+import '../styles/SideMenu.css';
 
-    const SideMenu = ({ closeMenu }) => {
-    return (
-        <>
-        {/* Overlay semitransparente que oscurece el fondo */}
-        <div className="menu-overlay" onClick={closeMenu}></div>
-        
-        {/* Menú lateral */}
-        <div className="side-menu">
-            <button className="close-button" onClick={closeMenu}>✕</button>
-            <ul>
-            <li><a href="/#">Encuentra un regalo</a></li>
-            <li><a href="/#">¿Qué es el dozo?</a></li>
-            <li><a href="/#">Cómo dar y recibir dozo</a></li>
-            <li><a href="/#">Leer la revista</a></li>
-            <li><a href="/#">Ver Illustrator</a></li>
-            <li><a href="/#">Iniciar sesión</a></li>
-            <li><a href="/#">Registro de nuevos miembros</a></li>
-            <li><a href="/#">Dónde comprar</a></li>
-            <li><a href="/#">Servicios corporativos</a></li>
-            <li><a href="/#">Preguntas frecuentes</a></li>
-            <li><a href="/#">Contáctenos</a></li>
-            </ul>
-            <div className="side-menu-icons">
-            <i className="fab fa-xing"></i>
-            <i className="fab fa-line"></i>
-            <i className="fab fa-instagram"></i>
-            </div>
-        </div>
-        </>
-    );
+const SideMenu = ({ closeMenu }) => {
+    const [isGiftMenuOpen, setIsGiftMenuOpen] = useState(false);
+
+    const toggleGiftMenu = () => {
+        setIsGiftMenuOpen(!isGiftMenuOpen);
     };
 
-    export default SideMenu;
+    return (
+        <>
+            <div className="menu-overlay" onClick={closeMenu}></div>
+            
+            <div className="side-menu">
+                <button className="close-button" onClick={closeMenu}>✕</button>
+                <ul className="main-menu">
+                    <li>
+                        <div className="menu-item" onClick={toggleGiftMenu}>
+                            <i className="bi bi-search menu-icon"></i>
+                            <span>Encuentra un estilo</span>
+                            <i className={`bi ${isGiftMenuOpen ? 'bi-chevron-down' : 'bi-chevron-right'} arrow`}></i>
+                        </div>
+                        <div className={`submenu ${isGiftMenuOpen ? 'open' : ''}`}>
+                            <div className="search-bar">
+                                <input type="text" placeholder="Búsqueda por palabra clave" />
+                            </div>
+                            <ul className="price-range">
+                                <li><a href="/#">Ver todos las categorias</a></li>
+                                <li><a href="/#">~29999cop</a></li>
+                                <li><a href="/#">30000~40000 cop</a></li>
+                                <li><a href="/#">50000~99999 cop</a></li>
+                                <li><a href="/#">100000 cop~</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="/about">
+                            <i className="bi bi-question-circle menu-icon"></i>
+                            ¿Qué es el dōzo?<i className="bi bi-chevron-right arrow"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/howto">
+                            <i className="bi bi-gift menu-icon"></i>
+                            Cómo usar dōzo<i className="bi bi-chevron-right arrow"></i>
+                        </a>
+                    </li>
+                </ul>
+                <ul className="secondary-menu">
+                    <li><a href="/login">Iniciar sesión</a></li>
+                    <li><a href="/register">Registro de nuevos miembros</a></li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default SideMenu;
