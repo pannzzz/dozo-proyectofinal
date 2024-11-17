2c22dfe47161f9cf39440acec91610d771280cea
pannzzz <maria.jimenez544@soy.sena.edu.co>
2024-11-16 22:05:59 -0500

    se hizo condicionales

--- dozo/src/components/SideMenu.js
+++ dozo/src/components/SideMenu.js
@@ -25,84 +25,85 @@ const SideMenu = ({ closeMenu }) => {
     return (
         <>
             <div className="menu-overlay" onClick={closeMenu}></div>
-            <div class="add-globalMenu__inner">
-            <div className="side-menu">
-                <button className="close-button" onClick={closeMenu}>✕</button>
-                <div className='add-globalMenu__contents'>
-                    <div className=''>
-                <ul className="add-globalMenu__list01">
-                    <li className='unu'>
-                    <div class="add-globalMenu__list__inner">
-                    <a href="/findgift" className="add-globalMenu__list01__link">
-                    <img 
-                        src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-search.svg" 
-                        alt="Buscar icono"/>
-                    <span> Encuentra un estilo</span>                                                      <i className="bi bi-chevron-right arrow"></i>
-                    </a>
+            <div className="add-globalMenu__inner">
+                <div className="side-menu">
+                    <button className="close-button" onClick={closeMenu}>✕</button>
+                    <div className="add-globalMenu__contents">
+                        <div>
+                            <ul className="add-globalMenu__list01">
+                                <li className="unu">
+                                    <div className="add-globalMenu__list__inner">
+                                        <a href="/findgift" className="add-globalMenu__list01__link">
+                                            <img
+                                                src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-search.svg"
+                                                alt="Buscar icono" />
+                                            <span> Encuentra un estilo</span>
+                                            <i className="bi bi-chevron-right arrow"></i>
+                                        </a>
+                                    </div>
+                                </li>
+
+                                <li className="unu2">
+                                    <a href="/about" className="add-globalMenu__list01__link">
+                                        <img
+                                            src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-question.svg"
+                                            alt="Ícono de pregunta"
+                                        />
+                                        <span>¿Qué es el dōzo?</span>
+                                        <i className="bi bi-chevron-right arrow"></i>
+                                    </a>
+                                </li>
+
+                                <li className="unu3">
+                                    <a href="/howto" className="add-globalMenu__list01__link">
+                                        <img
+                                            src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-magazine.svg"
+                                            alt="Ícono de regalo" className="imagen-regalo"
+                                        />
+                                        <span>Cómo usar dozo</span><i className="bi bi-chevron-right arrow"></i>
+                                    </a>
+                                </li>
+
+                                {user && ( // Solo mostrar si el usuario ha iniciado sesión
+                                    <>
+                                        <li className="unu3">
+                                            <a href="/favoritos" className="add-globalMenu__list01__link">
+                                                <img
+                                                    src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-brush.svg"
+                                                    alt="Ícono de favoritos" className="imagen-regalo"
+                                                />
+                                                <span>Tus favoritos</span><i className="bi bi-chevron-right arrow"></i>
+                                            </a>
+                                        </li>
+
+                                        <li className="unu3">
+                                            <a href="/pedidos" className="add-globalMenu__list01__link">
+                                                <img
+                                                    src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-present.png"
+                                                    alt="Ícono de pedidos" className="imagen-regalo"
+                                                />
+                                                <span>Tus pedidos</span><i className="bi bi-chevron-right arrow"></i>
+                                            </a>
+                                        </li>
+                                    </>
+                                )}
+                            </ul>
+                        </div>
+                        <ul className="secondary-menu">
+                            {user ? (
+                                <>
+                                    <li><span className="user-info">Hola, {user.username}</span></li>
+                                    <li><button className="logout-button" onClick={handleLogout}>Cerrar sesión</button></li>
+                                </>
+                            ) : (
+                                <>
+                                    <li><a href="/login">Iniciar sesión</a></li>
+                                    <li><a href="/register">Registro de nuevos miembros</a></li>
+                                </>
+                            )}
+                        </ul>
                     </div>
-                    </li>
-
-
-                    <li className='unu2'>
-                        <a href="/about" className="add-globalMenu__list01__link">
-                        <img 
-                            src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-question.svg" 
-                            alt="Ícono de pregunta" 
-                        />
-                        <span>¿Qué es el dōzo?</span>
-                        <i className="bi bi-chevron-right arrow"></i>
-                        </a>
-                    </li>
-
-
-                    <li className='unu3'>
-                    <a href="/howto" className="add-globalMenu__list01__link">
-                    <img 
-                        src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-magazine.svg" 
-                        alt="Ícono de regalo " className='imagen-regalo' 
-                    />
-                    <span>Cómo usar dozo</span><i className="bi bi-chevron-right arrow"></i>
-                    </a>
-                    </li>
-
-
-                    <li className='unu3'>
-                    <a href="/favoritos" className="add-globalMenu__list01__link">
-                    <img 
-                        src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-brush.svg" 
-                        alt="Ícono de regalo " className='imagen-regalo' 
-                    />
-                    <span>Tus favoritos</span><i className="bi bi-chevron-right arrow"></i>
-                    </a>
-                    </li>
-
-                    <li className='unu3'>
-                    <a href="/pedidos" className="add-globalMenu__list01__link">
-                    <img 
-                        src="https://auth.dozo-gift.com/front/v1_1/images/common/icon-present.png" 
-                        alt="Ícono de regalo " className='imagen-regalo' 
-                    />
-                    <span>Tus pedidos</span><i className="bi bi-chevron-right arrow"></i>
-                    </a>
-                    </li>
-                </ul>
                 </div>
-                <ul className="secondary-menu">
-                    {user ? (
-                        <>
-                            <li><span className="user-info">Hola, {user.username}</span></li>
-                            <li><button className="logout-button" onClick={handleLogout}>Cerrar sesión</button></li>
-                        </>
-                    ) : (
-                        <>
-                            <li><a href="/login">Iniciar sesión</a></li>
-                            <li><a href="/register">Registro de nuevos miembros</a></li>
-                        </>
-                    )}
-                </ul>
-                </div>
-
-            </div>
             </div>
         </>
     );
